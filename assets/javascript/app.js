3d0ce03759699c66040c7f0baa52306913bdfa0f
hnguy0221 <macbook@Macbooks-MacBook-Pro.local>
2017-06-10 20:59:05 -0600

    Fixed images incorrectly displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,9 +8,9 @@
 // Pseudocode  :
 //============================================================================
 var animalsArr = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", 
-        "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehob",
-        "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", 
-        "teacup pig", "serval", "salamander", "frog"];
+    "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehob",
+    "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", 
+    "teacup pig", "serval", "salamander", "frog"];
 
 //============================================================================
 // Name        : insertionSort
@@ -56,10 +56,12 @@ function seqOrderedSearch(searchItem)
     var i;
     var found = false;
     var location;
+    searchItem = searchItem.toLowerCase();//convert to lower case
 
     for (i = 0; i < animalsArr.length; i++)
     {
-        if (animalsArr[i] >= searchItem)
+        var animal = animalsArr[i].toLowerCase(); //convert to lower case
+        if (animal >= searchItem)
         {
             found = true;
             break;
@@ -67,7 +69,7 @@ function seqOrderedSearch(searchItem)
     }
     if (found)
     {
-        if (animalsArr[i] === searchItem)
+        if (animal === searchItem)
         {
             location = i;
         }
@@ -131,7 +133,8 @@ function reset()
 function displayAnimalInfo()
 {
     var animalStr;
-    var img_still;
+    var imgOriginalStill;
+    var imgOriginal;
     var rated;
     var animal = $(this).attr("data-name");
     var api_key = "dc6zaTOxFJmzC";
@@ -145,48 +148,53 @@ function displayAnimalInfo()
         method: "GET"
     }).done(function(response) {
         var len = response["data"].length;
-        //var len = 15;
+        //var len = 2;
         for (var i = 0; i < len; i++)
         {
             // Creates an element to hold the rating
             rated = response["data"][i].rating;
             console.log("Rating: " + rated);
             // Creates an element to hold the image
-            img_still = response["data"][i]["images"]["original_still"]["url"];
-            console.log("Image: " + img_still);
-            //The below logic is to display 1 image per column up 3 images per row.
+            imgOriginalStill = response["data"][i]["images"]["original_still"]["url"];
+            imgOriginal = response["data"][i]["images"]["original"]["url"];
+            console.log("Original Still Image: " + imgOriginalStill);
+            console.log("Original Image: " + imgOriginal);
+            //The below logic is to display 1 image per column up 4 images per row.
             if (i === 0)
             {
                 animalStr = "<div class='row'>";
-                animalStr += "<div col-xs-4 col-sm-4'>Rating: " + rated +
-                    "<img class='img-responsive thumbnail' src='" + img_still + "' /></div>";
+                animalStr += "<div class='col-xs-3 col-sm-3'>Rating: " + rated +
+                    "<img class='img-responsive thumbnail gif' src='" + imgOriginalStill + "' data-still='" + imgOriginalStill + 
+                    "' data-animate='" + imgOriginal + "' data-state='still' /></div>";
                 if (i === len-1)
                 {
                     animalStr += "</div>";
                 }
                 console.log("In if (i === 0): " + animalStr);
             }
-            else if (i % 3 !== 0)
+            else if (i % 4 !== 0)
             {
-                animalStr += "<div col-xs-4 col-sm-4'>Rating: " + rated +
-                    "<img class='img-responsive thumbnail' src='" + img_still + "' /></div>";
+                animalStr += "<div class='col-xs-3 col-sm-3'>Rating: " + rated +
+                    "<img class='img-responsive thumbnail gif' src='" + imgOriginalStill + "' data-still='" + imgOriginalStill + 
+                    "' data-animate='" + imgOriginal + "' data-state='still' /></div>";
                 if (i === len-1)
                 {
                     animalStr += "</div>";
                 }
-                console.log("In else if (i % 3 !== 0): " + animalStr);
+                console.log("In else if (i % 4 !== 0): " + animalStr);
             }
-            else if (i % 3 === 0) //display up to 3 images per row
+            else if (i % 4 === 0) //display up to 4 images per row
             {
                 animalStr += "</div>";
                 animalStr += "<div class='row'>";
-                animalStr += "<div col-xs-4 col-sm-4'>Rating: " + rated +
-                        "<img class='img-responsive thumbnail' src='" + img_still + "' /></div>";
+                animalStr += "<div class='col-xs-3 col-sm-3'>Rating: " + rated +
+                    "<img class='img-responsive thumbnail gif' src='" + imgOriginalStill + "' data-still='" + imgOriginalStill + 
+                    "' data-animate='" + imgOriginal + "' data-state='still' /></div>";
                 if (i === len-1)
                 {          
                     animalStr += "</div>";
                 }
-                console.log("In else if (i % 3 === 0): " + animalStr);
+                console.log("In else if (i % 4 === 0): " + animalStr);
             }
         }
         //var myJSON = JSON.stringify(response["data"]);
@@ -195,39 +203,82 @@ function displayAnimalInfo()
     });
 }
 
+//============================================================================
+// Name        : playGif
+// Author      : Hai Nguyen
+// Version     :
+// Copyright   : 2017
+// Description : This function unpauses and pauses a gif (image) when the user
+//               clicks on it.
+//============================================================================
+function playGif()
+{
+    //Check if the variable state is equal to 'still',
+    //then update the src attribute of this image to its data-animate value,
+    //and update the data-state attribute to 'animate'.
+    //If state does not equal 'still', then update the src attribute of this
+    //image to its data-still value and update the data-state attribute to 
+    //'still'
+    var dataState = $(this).attr("data-state");
+    if (dataState === "still")
+    {
+        var animatedSrc = $(this).attr("data-animate");
+        $(this).attr("src", animatedSrc);
+        $(this).attr("data-state", "animate");
+    }
+    else
+    {
+        var stillSrc = $(this).attr("data-still");
+        $(this).attr("src", stillSrc);
+        $(this).attr("data-state", "still");
+    }
+}
+
+function addAnimal()
+{
+    event.preventDefault();
+
+    // This line of code will grab the input from the textbox
+    var animal = $("#animal-input").val().trim();
+
+    // The movie from the textbox is then added to our array
+    if (animal === "")
+    {
+        alert("Please enter an animal!");
+    }
+    else if (seqOrderedSearch(animal) != -1)
+    {
+        alert("Animal " + animal + " already exists! Please enter a different animal!");
+    }
+    else
+    {
+        animalsArr.push(animal);
+    }
+
+    //reset the movie text input field to empty string
+    reset();
+
+    //calling renderButtons which handles the processing of our animals array
+    renderButtons();
+}
+
 $(document).ready(function()
 {
-    // This function handles events where the add animal button is clicked
-    $("#add-animal").on("click", function(event) 
-    {
-        event.preventDefault();
+    //This function handles events where the add animal button is clicked
+    //The reason this form works because the button with the #add-animal id
+    //was already loaded by the index.html.
+    $("#add-animal").on("click", addAnimal);
 
-        // This line of code will grab the input from the textbox
-        var animal = $("#animal-input").val().trim();
-
-        // The movie from the textbox is then added to our array
-        if (animal === "")
-        {
-            alert("Please enter an animal!");
-        }
-        else if (seqOrderedSearch(animal) != -1)
-        {
-            alert("Animal " + animal + " already exists! Please enter a different animal!");
-        }
-        else
-        {
-            animalsArr.push(animal);
-        }
-
-        //reset the movie text input field to empty string
-        reset();
-
-        // Calling renderButtons which handles the processing of our animals array
-        renderButtons();
-    });
-
-    // Adding click event listeners to all elements with a class of "animal"
+    //Adding click event listeners to all elements with a class of "animal"
+    //Need to use this form of click event listeners because the animal buttons
+    //are loaded dynamically.
     $(document).on("click", ".animal", displayAnimalInfo);
+
+    //adding click event listeners to all elements with a class of "gif"
+    //$(".gif").on("click", playGif); //this form will not work because gif images
+                                      //are loaded dynamically. Thus, we need the
+                                      //click event listeners form below. 
+    $(document).on("click", ".gif", playGif);
 
     //reset the animal text input field to empty string
     reset();
